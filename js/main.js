1d33348ba2a0cee6dc0e1055be22fb6fe17ac512
arinzeJeffrey <nnamenearinze@gmail.com>
2020-07-13 18:17:06 +0100

    working on a nerd blog web app

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100% ";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0% ";
+}
+
+
+function openSearch() {
+    document.getElementById("mySearch").style.width = "100% ";
+}
+
+function closeSearch() {
+    document.getElementById("mySearch").style.width = "0% ";
+}
+
+let test = document.getElementById("test")
+console.log(test.style.width)
